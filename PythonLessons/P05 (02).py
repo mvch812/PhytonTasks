c1884775e5ac33351430292ff37abd2774f2f7a6
MVCH812 <mvch812@gmail.com>
2022-01-30 18:10:56 +0300

    solved 02 for new taskslist

--- PythonLessons/P05 (02).py
+++ PythonLessons/P05 (02).py
@@ -21,20 +21,25 @@
 
 # 2.	Найти максимальное из пяти чисел
 import random
-k = random.randint(1, 100)
-b = random.randint(1, 100)
-c = random.randint(1, 100)
-print(k, b, c)
+Numbers = []
 
 
-def MaxNumber(k, b, c):
-
-    if b > k and b > c:
-        return (b)
-    elif c > k and c > b:
-        return (c)
-    else:
-        return (k)
+def FillNumbers(Numbers):
+    for i in range(5):
+        Numbers.append(random.randint(1, 100))
+    return Numbers
 
 
-print(f"максимальное из трех чисел:{MaxNumber(k, b, c)}")
+print(FillNumbers(Numbers))
+
+
+def MaxNumber(Numbers):
+    max = Numbers[0]
+    for i in range(5):
+        if Numbers[i] > max:
+            max = Numbers[i]
+    return max
+
+
+print(
+    f"Максимальное из случайных пяти чисел:{Numbers} равно: {MaxNumber(Numbers)}")
